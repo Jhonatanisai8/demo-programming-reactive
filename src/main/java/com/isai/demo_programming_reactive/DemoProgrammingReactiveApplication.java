3784430cfe368b293d63d9c4493668567b564a98
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-26 10:21:15 -0500

    init

---
+++ src/main/java/com/isai/demo_programming_reactive/DemoProgrammingReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_programming_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProgrammingReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProgrammingReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_programming_reactive/DemoProgrammingReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_programming_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProgrammingReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
